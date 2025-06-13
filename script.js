f75fc0b46cd6de3ba93b5f46e05a6161e829a8d7
risahyusuf <risahyusuf@gmail.com>
2025-06-13 16:30:37 +0100

    check

---
+++ script.js
@@ -0,0 +1,12 @@
+const reviews = [
+    {
+        image: "images/review image 1.png",
+        text: "I quickly found the right tour for me, but I had a few questions about the hotel, I wrote to tech support and they answered all my questions within an hour. The vacation itself was perfect. Thank you very much. I will come back again and again."
+        nameImage: "images/Jannike Borg, Publisher.png"
+    },
+]
+
+
+
+
+I quickly found the right tour for me, but I had a few questions about the hotel, I wrote to tech support and they answered all my questions within an hour. The vacation itself was perfect. Thank you very much. I will come back again and again.
